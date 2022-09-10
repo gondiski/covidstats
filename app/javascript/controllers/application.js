15189a89c3e3b2c7d013f3aa4beff0156a4edccf
Enoch Otieno <enochotieno@rocketmail.com>
2022-09-10 18:25:25 +0300

    Add the javascript libraries

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,3 +7,8 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
+
+import jquery from "jquery";
+import toastr from "toastr";
+import flatpickr from "flatpickr";
+import SweetAlert from "sweetalert";
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "jquery", to: "./node_modules/jquery"
+pin "toastr", to: "./node_modules/toastr"
+pin "sweetalert", to: "./node_modules/sweetalert2"
+pin "flatpickr",to: "./node_modules/flatpickr"
